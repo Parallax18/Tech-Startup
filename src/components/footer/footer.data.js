e03f15c0236850ee31f3a76ced682692cf7c81a0
Parallax18 <rhythmist204@gmail.com>
2022-04-07 10:23:21 +0100

    feat: home page with all sections built and functional

---
+++ src/components/footer/footer.data.js
@@ -0,0 +1,24 @@
+export default {
+  menuItems: [
+    {
+      path: '/',
+      label: 'Home',
+    },
+    {
+      path: '/',
+      label: 'Adversite',
+    },
+    {
+      path: '/',
+      label: 'Supports',
+    },
+    {
+      path: '/',
+      label: 'Marketing',
+    },
+    {
+      path: '/',
+      label: 'Contact',
+    },
+  ],
+};
---
+++ src/components/section-header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box, Text, Heading } from 'theme-ui';
+
+export default function SectionHeader({ title, slogan, isWhite }) {
+  return (
+    <Box sx={{variant: 'sectionHeader'}}>
+      <Text 
+      sx={{
+        variant: 'sectionHeader.subTitle',
+        color: isWhite ? 'white' : 'primary',
+        opacity:isWhite ? 0.7 : 1,
+      }}
+      as="p">
+        {slogan}
+      </Text>
+      <Heading
+        sx={{
+          variant: 'sectionHeader.title',
+          color: isWhite ? 'white' : 'heading',
+        }}
+        as="h2"
+      >
+        {title}
+      </Heading>
+    </Box>
+  );
+}
